38e2ba58f31e8a9dcefa0173bd1ceccbc67f81e9
MyDXD <dislove107@gmail.com>
2024-09-12 14:06:12 +0700

    connect db & api user get and post

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userSchema = require("../models/userModel");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// ดูผู้ใช้ทั้งหมด
+router.get("/", async (req, res) => {
+  try {
+    const users = await userSchema.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await userSchema.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// สร้างผู้ใช้ใหม่
+router.post('/', async (req, res) => {
+  try {
+    const newUser = new userSchema(req.body);
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
