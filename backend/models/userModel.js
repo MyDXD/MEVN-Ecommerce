9e0b53ba1452c5f27f47b8ad00df00899fe4c3ea
Hassadin Sasoh <dislove107@gmail.com>
2024-09-12 20:40:53 +0700

    สร้าง api register & login
    -ทำการเพิ่ม role ใน model user เพื่อตรวจสอบสิทธิ์ user ในอนาคต
    -เพื่ม timestam  ใน model user
    -สร้าง api register และ hash password
    -สร้าง api login และทำการ return token

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,36 +1,42 @@
 // models/userModel.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the User Schema
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+    address: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var router = express.Router();
+const userSchema = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+const secret = "mysecret"
+
+
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password, age, address } = req.body;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = new userSchema({
+      username,
+      email,
+      password: hashPassword,
+      age,
+      address,
+    });
+    await user.save();
+    res
+      .status(201)
+      .json({
+        status: "201",
+        message: "User registered successfully",
+        data: user,
+      });
+  } catch (error) {
+    res.status(500).json({ message: error.toString() });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // ค้นหาผู้ใช้ในฐานข้อมูล
+    const user = await userSchema.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ status : 404 ,message: "User not found" });
+    }
+
+    // ตรวจสอบรหัสผ่าน
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ status : 401 ,message: "Invalid password" });
+    }
+
+    // สร้าง JWT token
+    const token = jwt.sign(
+      {  id: user._id, role: user.role, name : user.username },
+      secret,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // res.json({ id : user._id , email : user.email, userName : user.name, token ,   isActive : user.isActive });
+    res.status(200).json({ message : "Login Successfuly" , token : token,  email : user.email , username : user.username });
+  } catch (error) {
+    res.status(500).json({ message: error.toString() });
+  }
+});
+module.exports = router;
