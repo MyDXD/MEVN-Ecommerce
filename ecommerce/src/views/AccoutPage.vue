c552ed277afddd52a55ac94a38ae42f86215858c
Nugup X <165800288+PugunP@users.noreply.github.com>
2024-09-16 16:03:11 +0700

    add demo product and bucket

--- ecommerce/src/views/AccoutPage.vue
+++ ecommerce/src/views/AccoutPage.vue
@@ -1,3 +1,54 @@
 <template>
-    <div>HELLO ACCOUNT</div>
-</template>
+    <v-container>
+      <v-row>
+        <v-col
+          v-for="(product, index) in products"
+          :key="index"
+          cols="12"
+          sm="6"
+          md="4"
+        >
+          <v-card>
+            <v-card-title>{{ product.title }}</v-card-title>
+            <v-card-subtitle>{{ product.description }}</v-card-subtitle>
+            <v-card-text>
+              <div>Quantity: {{ product.quantity }}</div>
+              <div>Price: {{ product.price }} ฿</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="primary" @click="buyProduct(product)">Buy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+  
+<script>
+/* eslint-disable */
+import axios from "axios";
+  export default {
+    data() {
+      return {
+        products: [],
+      };
+    },
+    methods: {
+      async fetchProducts() {
+        try {
+          const response = await axios.get("http://localhost:5000/products");
+          this.products = response.data;
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        }
+      },
+      buyProduct(product) {
+        console.log(`Buying product: ${product.title}`);
+      },
+    },
+    created() {
+      this.fetchProducts();
+    },
+  };
+  </script>
+  
