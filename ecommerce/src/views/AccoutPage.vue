736a92ae1228641828113acb6df883f078be7c14
Nugup X <165800288+PugunP@users.noreply.github.com>
2024-09-17 13:27:01 +0700

    add image

--- ecommerce/src/views/AccoutPage.vue
+++ ecommerce/src/views/AccoutPage.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <h1>ALL PRODUCTS</h1>
-        <v-container>
+  <div>
+    <h1>ALL PRODUCTS</h1>
+    <v-container>
       <v-row>
         <v-col
           v-for="(product, index) in products"
@@ -9,8 +9,13 @@
           cols="12"
           md="4"
         >
-          <div class="card">
-            <v-card>
+          <v-card class="mx-auto my-4" max-width="400px">
+            <v-img
+              :src="product.image"
+              height="200px"
+              class="white--text align-end"
+            >
+            </v-img>
             <v-card-title>{{ product.title }}</v-card-title>
             <v-card-subtitle>{{ product.description }}</v-card-subtitle>
             <v-card-text>
@@ -18,50 +23,50 @@
               <div>Price: {{ product.price }} ฿</div>
             </v-card-text>
             <v-card-actions>
+              <v-spacer></v-spacer>
               <v-btn color="primary" @click="buyProduct(product)">Buy</v-btn>
             </v-card-actions>
           </v-card>
-          </div>
         </v-col>
       </v-row>
     </v-container>
-    </div>
-  </template>
-  
+  </div>
+</template>
+
 <script>
 /* eslint-disable */
 import axios from "axios";
-  export default {
-    data() {
-      return {
-        products: [],
-      };
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    async fetchProducts() {
+      try {
+        const response = await axios.get("http://localhost:5000/products");
+        this.products = response.data;
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
     },
-    methods: {
-      async fetchProducts() {
-        try {
-          const response = await axios.get("http://localhost:5000/products");
-          this.products = response.data;
-        } catch (error) {
-          console.error("Error fetching products:", error);
-        }
-      },
-      buyProduct(product) {
-        console.log(`Buying product: ${product.title}`);
-      },
+    buyProduct(product) {
+      console.log(`Buying product: ${product.title}`);
     },
-    created() {
-      this.fetchProducts();
-    },
-  };
-  </script>
-  <style scoped>
-  h1 {
+  },
+  created() {
+    this.fetchProducts();
+  },
+};
+</script>
+
+<style scoped>
+h1 {
   text-align: center;
   margin-block: 2rem;
 }
-.card{
-    margin-block: 2rem;
+.card {
+  margin-block: 2rem;
 }
 </style>
-  
